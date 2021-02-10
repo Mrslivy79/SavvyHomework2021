b6bdae9385f8bce91c655ddaca85584f5432415e
Mrslivy79 <mrslivy79@yahoo.com>
2021-02-10 16:39:36 -0600

    add update to wWeek 2 Homework

---
+++ Week2Homework/Homeworkweek2.js
@@ -0,0 +1,14 @@
+let pizzaPlace;
+let numberOfToppings;
+
+pizzaPlace = "Toastys";
+numberOfToppings = 3;
+
+Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
+`${pizzaPlace} is best for their ${numberOfToppings} pizzas`;
+
+Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
+
+Add, commit, and push your JS file to your GitHub repo.
+
+Send your TA a link to your GitHub Homework repo when finished.
