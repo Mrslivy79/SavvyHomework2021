c76268d984a60bf77a99f89e78d94ec24718906d
Mrslivy79 <mrslivy79@yahoo.com>
2021-02-10 16:40:53 -0600

    add update to Week 2 Homework

--- Week2Homework/Homeworkweek2.js
+++ Week2Homework/Homeworkweek2.js
@@ -1,14 +1,20 @@
 let pizzaPlace;
 let numberOfToppings;
 
-pizzaPlace = "Toastys";
-numberOfToppings = 3;
+pizzaPlace = Pie House
+numberOfToppings = 3
 
-Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
-`${pizzaPlace} is best for their ${numberOfToppings} pizzas`;
+console.log(pizzaPlace, typeof pizzaPlace); 
+console.log(numberOfToppings, typeof numberOfToppings);
 
-Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
+`The ${pizzaPlace} is known for ${numberOfToppings} topping pies.`
 
-Add, commit, and push your JS file to your GitHub repo.
+if (conditionnumerOfToppings = 5) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
 
-Send your TA a link to your GitHub Homework repo when finished.
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
