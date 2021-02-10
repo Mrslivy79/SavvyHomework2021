d3a8d9ecbbeb1ac4565245f3b0960489f69d5538
Mrslivy79 <mrslivy79@yahoo.com>
2021-02-10 17:45:15 -0600

    adjusted Homework week 2

--- Week2Homework/Homeworkweek2.js
+++ Week2Homework/Homeworkweek2.js
@@ -1,7 +1,7 @@
 let pizzaPlace;
 let numberOfToppings;
 
-pizzaPlace = Pie House
+pizzaPlace = "Pie House"
 numberOfToppings = 3
 
 console.log(pizzaPlace, typeof pizzaPlace); 
@@ -9,12 +9,18 @@ console.log(numberOfToppings, typeof numberOfToppings);
 
 `The ${pizzaPlace} is known for ${numberOfToppings} topping pies.`
 
-if (conditionnumerOfToppings = 5) {
+if (numberOfToppings = 5) {
   console.log("Quality, not quantity.");
 } else {
   console.log("A whole lot of pizza.");
 }
 
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
+i= 0;
+for (let i = 0; i <= 10; i++) {
+    if (i === 0) {
+        console.log(i +  " is even");
+    }
+    else (i % 2 === 0) {
+        console.log(i + " is even");
+    }    
 }
