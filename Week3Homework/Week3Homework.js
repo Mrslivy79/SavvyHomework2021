dbb5e546e5c0e45f7bb03bd1e70853b8744f0606
Mrslivy79 <mrslivy79@yahoo.com>
2021-02-14 13:26:41 -0600

    new corrected assignment file

---
+++ Week3Homework/Week3Homework.js
@@ -0,0 +1,27 @@
+let pizzaToppings = ["beef", "bacons", "onions", banana peppers];
+
+function greetCustomer()
+{
+    "Welcome to Pizza House, our toppings are: 
+    for(let topping of pizzaToppings) {
+      greeting += `${topping}, `};
+}
+greetCustomer()
+
+function getPizzaOrder(size, crust, ....toppings){
+  let order = `One ${size} ${crust} crust pizza with `
+  for(let topping of pizzaToppings) {order += `${toppings}, `;
+}
+console.log(`${order}, coming up!`);
+return [size, crust, toppings];
+}
+getPizzaOrder("Large", "Hand-Tossed", "Toppings")
+
+function preparePizza ([orderSize, orderCrust, orderToppings]){
+console.log(""...CookingPizza..."")
+return { size: orderSize,
+crust: orderCrust,
+toppings: orderToppings
+};
+}
+preparePizza();
