ae836d2d56c68f865ef12a819a835d06fd815477
Mrslivy79 <mrslivy79@yahoo.com>
2021-02-21 00:44:38 -0600

    Week3Homeweek.js

--- Week3Homework/Week3Homework.js
+++ Week3Homework/Week3Homework.js
@@ -1,27 +1,28 @@
-let pizzaToppings = ["beef", "bacons", "onions", banana peppers];
+let pizzaToppings = ["beef", "bacons", "onions", "banana peppers"];
 
 function greetCustomer()
 {
-    "Welcome to Pizza House, our toppings are: 
+    let greeting = "Welcome to Pizza House, our toppings are"; 
     for(let topping of pizzaToppings) {
       greeting += `${topping}, `};
 }
 greetCustomer()
 
-function getPizzaOrder(size, crust, ....toppings){
+function getPizzaOrder(size, crust, ...toppings){
   let order = `One ${size} ${crust} crust pizza with `
-  for(let topping of pizzaToppings) {order += `${toppings}, `;
+  for(let topping of pizzaToppings) {order += `${topping}, `;
 }
 console.log(`${order}, coming up!`);
 return [size, crust, toppings];
 }
-getPizzaOrder("Large", "Hand-Tossed", "Toppings")
+let pizzaOrder = getPizzaOrder("Large", "Hand-Tossed", "Toppings")
 
 function preparePizza ([orderSize, orderCrust, orderToppings]){
-console.log(""...CookingPizza..."")
+console.log("...CookingPizza...")
 return { size: orderSize,
 crust: orderCrust,
 toppings: orderToppings
+
 };
 }
-preparePizza();Week3
+preparePizza(pizzaOrder);
